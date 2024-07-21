f9328dc9a79fd43db15d9e2cd02876be0bf636e1
ipe <izaki.yb1998@gmail.com>
2024-07-22 07:02:35 +0900

    dotenv

--- backend/src/database/database.go
+++ backend/src/database/database.go
@@ -1,16 +1,22 @@
 package database
 
 import (
+	"log"
 	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 var Db *gorm.DB
 
 func Connect() {
-	dsn := os.Getenv("DSN")
 	var err error
+	err = godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatal(err)
+	}
+	dsn := os.Getenv("DSN")
 	Db, err = gorm.Open(mysql.Open(dsn))
 	if err != nil {
 		panic(err)
